bdb8b04a465cf539d0164e313d71d76e7fd6d3b2
Tomas Garcia <tgarciaracchi@gmail.com>
2023-11-01 08:46:09 +0000

    Going well!

---
+++ models/intermediate/join_all_tables.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM 
+{{ref('stg_gz_raw_data__raw_gz_product')}}
+JOIN
+{{ref('stg_gz_raw_data__raw_gz_sales')}}
+USING (products_id)
+JOIN 
+{{ref('stg_gz_raw_data__raw_gz_ship')}}
+USING (orders_id)
